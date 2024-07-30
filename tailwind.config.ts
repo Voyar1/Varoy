c00d7a0b21d7f29edc9f35b49d97e125e293b729
Arkadiusz <areq12x@gmail.com>
2024-07-30 17:07:41 +0200

    Init commit. Crated extended colors for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,24 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        dark: {
+          100: "#000000",
+          200: "#070707",
+        },
+        light: {
+          900: "#FFFFFF",
+          800: "#F5F5F7",
+          850: "#CBCBCB",
+        },
       },
     },
   },
